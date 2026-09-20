0a806856f4761c4bf7f818cb22938ccacc873764
Daya230 <dutpaevad@gmail.com>
2026-09-20 12:43:04 +0500

    Add character processing logic in a.cpp
    Implement a solution that processes characters and outputs the first non-repeating character or -1.

--- lab2/a.cpp
+++ lab2/a.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <queue>
 
+using namespace std;
+
+void solve(){
+    int n;
+    cin>> n;
+
+    queue<char> q;
+    vector<int> cnt(26,0);
+
+    for(int i = 0 ; i< n ; i++){
+        char ch;
+        cin>> ch;
+
+        cnt[ch - 'a']++;
+        q.push(ch);
+
+        while(!q.empty()&& cnt[q.front()-'a']>1){
+            q.pop();
+        }
+
+        if (!q.empty()){
+            cout<< q.front();
+        }
+        else{
+            cout << -1;
+        }
+        if (i< n -1){
+            cout<< " ";
+        }
+    }
+    cout<< endl;
+}
+
+int main(){
+    int a;
+    cin>> a;
+    while(a--){
+        solve();
+    }
+    return 0;
+}
